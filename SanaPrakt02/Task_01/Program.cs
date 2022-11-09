7532ff2fc78abd381837ee020133c884e5f0d092
Денис Бордюг <ipz224_bdv@student.ztu.edu.ua>
2022-11-10 01:00:44 +0200

    Creat branch second

--- SanaPrakt02/Task_01/Program.cs
+++ SanaPrakt02/Task_01/Program.cs
@@ -7,6 +7,8 @@ namespace Task_01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
